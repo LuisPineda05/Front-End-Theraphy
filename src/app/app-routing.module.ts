bb9bf0e12e1d8b6c287cf400e32e4b03f2651c46
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2022-09-28 10:44:29 -0500

    feat(medical-appointments): added Medical Appointments component and paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import {PhysiotherapistsComponent} from "./user-type/patient/pages/physiotherapi
 import {PaymentsComponent} from "./user-type/patient/pages/payments/payments.component";
 import {ScheduleAppointmentsComponent} from "./user-type/patient/pages/schedule-appointments/schedule-appointments.component";
 import {TreatmentsComponent} from "./user-type/patient/pages/treatments/treatments.component";
+import {
+  MedicalAppointmentsComponent
+} from "./user-type/patient/pages/medical-appointments/medical-appointments.component";
+import {VideoSessionsComponent} from "./user-type/patient/pages/video-sessions/video-sessions.component";
 
 const routes: Routes = [
   {path:'physiotherapists', component: PhysiotherapistsComponent},
   {path:'treatments/:id', component: TreatmentsComponent},
   {path:'payments', component: PaymentsComponent},
-  {path:'schedule-appointments', component: ScheduleAppointmentsComponent}
+  {path:'schedule-appointments', component: ScheduleAppointmentsComponent},
+  {path: 'medical-appointments', component: MedicalAppointmentsComponent},
+  {path: 'video-sessions/:id', component: VideoSessionsComponent},
+
 ];
 
 @NgModule({
